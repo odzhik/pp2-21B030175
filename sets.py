2d19992443ec31b7b59a887fd42df51b78d03947
Olzhas Zhakayev <101634916+odzhik@users.noreply.github.com>
2023-02-04 23:58:53 +0600

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,23 @@
+fruits = {"apple", "banana", "cherry"}
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+#ex 2
+
+fruits = {"apple", "banana", "cherry"}
+fruits.add("orange")
+
+#ex 3
+
+fruits = {"apple", "banana", "cherry"}
+more_fruits = ["orange", "mango", "grapes"]
+fruits.update(more_fruits)
+
+#ex 4
+
+fruits = {"apple", "banana", "cherry"}
+fruits.remove("banana")
+
+#ex 5
+
+fruits = {"apple", "banana", "cherry"}
+fruits.discard("banana")
