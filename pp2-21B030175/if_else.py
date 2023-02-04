5be7cd16df8d0397ae23a2c0e509e3b15f8bd843
Olzhas Zhakayev <101634916+odzhik@users.noreply.github.com>
2023-02-05 00:16:03 +0600

    Add files via upload

---
+++ pp2-21B030175/if_else.py
@@ -0,0 +1,48 @@
+a = 50
+b = 10
+if a > b:
+    print("Hello World")
+
+#EX 2
+a = 50
+b = 10
+if a != b:
+    print("Hello World")
+
+#ex 3
+a = 50
+b = 10
+if a == b:
+    print("Yes")
+else:
+    print("No")
+
+#ex 4
+a = 50
+b = 10
+if a == b:
+    print("1")
+elif a > b:
+    print("2")
+else:
+    print("3")
+
+#ex 5
+
+if a == b and c == d:
+    print("Hello")
+
+#ex 6
+if a == b or c == d:
+    print("Hello")
+#ex 7
+if 5 > 2:
+    print("Five is greater than two!")
+#ex 8 
+if 5 > 2: print("Five is greater than two!")
+
+#ex 9
+if 5 > 2:
+    print("Yes")
+else
+    print("No")
---
+++ pp2-21B030175/variables.py
@@ -0,0 +1,27 @@
+carname = "Volvo"
+
+#ex 2
+
+x = 50
+
+#ex 3
+x = 5
+y = 10
+print(x + y)
+
+#ex 4
+x = 5
+y = 10
+z = x + y
+print(z)
+
+#ex 5
+myfirst_name = "John" 
+
+#ex 6 
+x = y = z = "orange"
+
+#ex 7 
+def myfunc():
+  global x
+  x = "fantastic"
